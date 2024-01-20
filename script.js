d30cd9577de035ef658fb5971fbca421ab5540de
armyjojo <jozereau.jonathan@gmail.com>
2024-01-20 11:16:40 +0100

    exo 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* EXERCICE 1 */
-function exo() {
+function carre() {
     var a=document.getElementById("nbr1").value;
     if(!isNaN(a))
     document.getElementById("nbr2").value=(Math.pow(a, 2));
@@ -15,3 +15,75 @@ function addition() {
     nbr5 = nbr3 + nbr4;
     document.getElementById("nbr5").value=nbr5;
 }
+
+/* EXERCICE 3 */
+
+function nbpair() {
+    var nbr8 = document.getElementById("nbr6").value;
+    var nbr7=0, nbr9=[], nbr10=nbr8+1;
+    while(nbr7++<nbr8){ !(nbr7&1) && nbr9.push(nbr7)};
+    return document.getElementById("nbr7").value=(nbr9.join('-'));
+}
+
+/* EXERCICE 4 */
+
+function premiers() {
+    var nbr11=[],nbr12,nbr13,compte=0;
+    var nbr12=1;
+    var nbr13=Number(document.getElementById("nbr14").value);
+    
+    if(nbr13>nbr12){
+    for(var nbr15=nbr12;nbr15<=nbr13;nbr15++){
+    for(var nbr16=1;nbr16<=nbr15;nbr16++){
+    if(nbr15%nbr16==0){
+    compte=compte+1;
+    }
+    }
+    if(compte==2){
+    nbr11.push(nbr15);
+    }
+    compte=0;
+    }
+    document.getElementById("nbr9").value=(nbr12+" et "+nbr13+" : "+nbr11.join(' ; '));
+    }
+    else{
+    document.getElementById("nbr9").value=("Impossible !");
+    }}
+
+    /* EXERCICE 5 */
+
+    function multiplication(nbr20)
+        {
+        var nbr20=Number(document.getElementById("nbr17").value);
+        var nbr21= ("nbr18");
+        var nbr19;
+        for (nbr19=0; nbr19<=10; nbr19++) {
+            nbr21=nbr20*nbr19;
+            document.getElementById("nbr18").innerHTML+=(nbr20 + " x " + nbr19 + " = " + nbr20 * nbr19 + " ; ");
+            }
+        }
+
+    /* EXERCICE 6 */
+
+    function fact(nbr22)
+        {
+        var nbr22=Number(document.getElementById("nbr23").value);
+        var nbr24, nbr22, nbr25 = 1;
+        for(nbr24 = 1; nbr24 <= nbr22; nbr24++)  
+        {
+            nbr25 = nbr25 * nbr24;
+        }  
+        return document.getElementById("nbr26").value+= "Pour le nombre " + nbr22 + " la factorielle est " + nbr25 + ". ";
+        }
+
+    /* EXERCICE 7 */
+
+    function invers(nbr27)
+        {
+        var nbr27=Number(document.getElementById("nbr28").value);
+        var nbr29 = 1;
+        var nbr30 = nbr29 / nbr27;
+    return document.getElementById("nbr31").value+= "Pour le nombre " + nbr27 + " son inverse est " + nbr30 + " . ";
+    }  
+
+    /* EXERCICE 8 => nbr32 */
